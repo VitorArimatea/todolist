e3ed2747ce8fe08cd9a3c1fe53c80e8c535f5aac
VitorArimatea <vitorarimatea@hotmail.com>
2025-04-17 14:45:08 -0300

    test: create success and failure tests for post lists

--- src/test/java/br/com/vitorarimatea/todolist/TodolistApplicationTests.java
+++ src/test/java/br/com/vitorarimatea/todolist/TodolistApplicationTests.java
@@ -1,13 +1,45 @@
 package br.com.vitorarimatea.todolist;
 
+import br.com.vitorarimatea.todolist.entity.Todo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TodolistApplicationTests {
+    @Autowired
+    private WebTestClient webTestClient;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void testCreateTodoSuccess() {
+        var todo = new Todo("Tarefa 1", "Descrição 1", true, 1);
 
+        webTestClient
+                .post()
+                .uri("/todos")
+                .bodyValue(todo)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$").isArray()
+                .jsonPath("$.length()").isEqualTo(1)
+                .jsonPath("$[0].nome").isEqualTo(todo.getNome())
+                .jsonPath("$[0].descricao").isEqualTo(todo.getDescricao())
+                .jsonPath("$[0].realizado").isEqualTo(todo.isRealizado())
+                .jsonPath("$[0].prioridade").isEqualTo(todo.getPrioridade());
+
+    }
+
+    @Test
+    void testCreateTodoFailure() {
+        webTestClient
+                .post()
+                .uri("/todos")
+                .bodyValue(
+                        new Todo("", "", false, 0)
+                )
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
 }
